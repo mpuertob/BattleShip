7633cd2f85af3d8ffabd95863ec8bd98677c971e
programacion <49229873+mpuertob@users.noreply.github.com>
2020-10-14 20:45:08 +0200

    con la clase Flota

--- src/modelo/flota.ts
+++ src/modelo/flota.ts
@@ -1,8 +1,23 @@
 import { Barco } from "./Barco";
+import { Dimension } from "./Dimension";
+import { GeneradorFlota } from "./generadorFlota";
 
 /*Es la clase
 que contiene todos los barcos 
 */
 export class Flota {
-  constructor(private _flota: Barco[] = []) {}
+  private _generadorFlota: GeneradorFlota;
+  private _flota: Barco[] = [];
+  constructor(private _dimension: Dimension) {
+    this._generadorFlota = new GeneradorFlota(this._dimension);
+    this.crearFlotas();
+  }
+
+  private crearFlotas(): void {
+    const tamanoBarcos = [2, 2, 2, 2, 3, 3, 3, 4, 4];
+    for (let i = 0; i < tamanoBarcos.length; i++) {
+      let barco: Barco = this._generadorFlota.crearBarco(tamanoBarcos[i]);
+      this._flota.push(barco);
+    }
+  }
 }
